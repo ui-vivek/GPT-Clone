ee46c9c746ba73746f4029455bc6becce407fde8
vivek singh <cu.vivek.1557@gmail.com>
2023-03-26 13:43:53 +0530

    Adding Sign In , Sing Up and Logout to the Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 const Navbar = () => {
+  const Navigate = useNavigate();
+  const loggedIn = JSON.parse(localStorage.getItem("authToken"));
+  //handleLogout
+  const handleLogout = async () => {
+    try {
+      await axios.post("/api/v1/auth/logout");
+      localStorage.removeItem("authToken");
+      Navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <Link to={'/'} className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+        <Link to={"/"} className="btn btn-ghost normal-case text-xl">
+          daisyUI
+        </Link>
       </div>
       <div className="flex-none px-4">
-        <Link to={'/register'}>
-        <button className="btn btn-circle btn-ghost px-2">
-          <i class="bx bxs-user-circle bx-md"></i>
-        </button>
-        </Link>
-        <Link to={'/login'}>
-          <button className="btn btn-circle btn-ghost px-2">
-            <i class="bx bxs-log-in-circle bx-md"></i>
-          </button>
-        </Link>
+        {loggedIn ? (
+          <Link to={"/login"} onClick={handleLogout}>
+            <button className="btn btn-circle btn-ghost px-2">LogOut</button>
+          </Link>
+        ) : (
+          <>
+            <Link to={"/register"}>
+              <button className="btn btn-circle btn-ghost px-2">Sign Up</button>
+            </Link>
+            <Link to={"/login"}>
+              <button className="btn btn-circle btn-ghost px-2">Sign In</button>
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
